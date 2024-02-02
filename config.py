2095fe99f11f9e358bda5a48ce33efad782a75ce
Ravi7019 <156110174+Ravi7019@users.noreply.github.com>
2024-02-02 09:42:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ class Var(object):
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "120"))  # 2 minutes
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     if 'DYNO' in environ:
